61d665473ec1336c64dcf57d53c6efe646d9d43d
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-24 20:50:47 -0500

    Initial Commit

---
+++ DemoColorSchemePreference20230224/DemoColorSchemePreference20230224App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoColorSchemePreference20230224App.swift
+//  DemoColorSchemePreference20230224
+//
+//  Created by Xavier on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoColorSchemePreference20230224App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
